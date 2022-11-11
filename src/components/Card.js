bee40383e94f85d5a3b0508e2c4a6311e78219b6
Ritu Thakur <105920415+Ritu-03Thakur@users.noreply.github.com>
2022-11-11 13:25:36 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,15 @@
+   import "./Card.css" ; 
+
 function Card(props){
  
     return(
         <>
+        <div className="rainbow">
         <div className="card " >
         <img className="card-top" src= {props.img} alt = "icon" height="100" width="100" />
           <h5 className="card-title">{props.title}</h5>
       </div>
+        </div>
         </>
     );
 }
