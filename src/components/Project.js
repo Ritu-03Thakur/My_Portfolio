31f03afa9b1ddfe021a146f09475071f0e8c1acf
Ritu Thakur <105920415+Ritu-03Thakur@users.noreply.github.com>
2022-11-11 13:20:44 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,6 +10,8 @@ export default function Project() {
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
         </div>
       </div>
     </>
