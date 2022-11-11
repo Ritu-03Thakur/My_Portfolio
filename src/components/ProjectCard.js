2d7ef2c7e632ecf36409f26eacf8f2d1c9f95bd2
Ritu Thakur <105920415+Ritu-03Thakur@users.noreply.github.com>
2022-11-11 13:17:28 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,23 +1,32 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import "./ProjectCard.css";
 import CardActions from '@mui/material/CardActions';
 // import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import CardMedia from "@mui/material/CardMedia";
 import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
 import wallpaper from "./img/wallpaper.jpg";
 
 export default function ProjectCard() {
   return (
-    <Card sx={{ maxWidth: 345  , margin : 5 , boxShadow: 5  }}>
+    <Card className="view view-first" sx={{ margin: 5, boxShadow: 5 }}>
       <CardMedia
+        className="img"
         component="img"
         alt="green iguana"
-        height="140"
-        image= {wallpaper} />
-      <CardActions>
-        <Button size="small">Link </Button>
-      </CardActions>
+        image={wallpaper}
+      />
+      <div className="mask">
+        <h2>Calculator</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          modi quos 
+        </p>
+        <CardActions>
+          <Button className="info" size="medium" variant="contained"  >Read More</Button>
+        </CardActions>
+      </div>
     </Card>
   );
 }
